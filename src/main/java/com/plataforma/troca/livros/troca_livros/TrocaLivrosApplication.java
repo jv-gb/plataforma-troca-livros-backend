0985c24c5c93733158784b9eeb1d023435b4aa87
Your Name <joaovgbv@gmail.com>
2025-02-26 10:56:53 -0300

    first commit

---
+++ src/main/java/com/plataforma/troca/livros/troca_livros/TrocaLivrosApplication.java
@@ -0,0 +1,13 @@
+package com.plataforma.troca.livros.troca_livros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrocaLivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrocaLivrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plataforma/troca/livros/troca_livros/TrocaLivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plataforma.troca.livros.troca_livros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrocaLivrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
